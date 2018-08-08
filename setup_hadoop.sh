cac255df7594ba808f8c5b196cd266dbd87bc6d0
piko.bu <pmng90@gmail.com>
2018-08-08 14:39:42 +0530

    first commit

--- setup_hadoop.sh
+++ setup_hadoop.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-#~ make this script executable by using chmod +x setup_hadoop.sh
-
-#~ feel free to change and adjust to your liking. 
-#~ The author is not responsible for the damage that has been
-#~ inflicted due to any misconfiguration or other mishaps
-#~ in short if it breaks you are on your own.. Sorry about that
-
 #title           :setup_hadoop.sh
 #description     :This script helps to configure hadoop 
 #date            :20180112
